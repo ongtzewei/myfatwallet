80c194e91967946f2da6d7f867a211ce6d1ea20f
Ong Tze Wei <ong.tzewei@gmail.com>
2020-05-21 13:39:44 +0800

    Update local_settings.py

--- myfatwallet/local_settings.py
+++ myfatwallet/local_settings.py
@@ -12,9 +12,9 @@ SECRET_KEY = '46ryv($lnuq6%n4y(o@===$p21#yuvogd4j$fziq457p)0q&o8'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'MyFatWalletDB',                      # Or path to database file if using sqlite3.
-        'USER': 'MyFatWallet',                      # Not used with sqlite3.
-        'PASSWORD': 'MyFatWallet',                  # Not used with sqlite3.
+        'NAME': 'XXX',                      # Or path to database file if using sqlite3.
+        'USER': 'XXX',                      # Not used with sqlite3.
+        'PASSWORD': 'XXX',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
         'ATOMIC_REQUESTS': True,
@@ -32,22 +32,17 @@ HAYSTACK_CONNECTIONS = {
 ALLOWED_HOSTS = ('.cronuts.com',)
 
 # used by django-hosts
-PARENT_HOST = 'cronuts.com:8080'
+PARENT_HOST = ''
 
 # used by django-cors-headers
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_REPLACE_HTTPS_REFERER = False
-CORS_ORIGIN_WHITELIST = (
-    'cronuts.com:8080',
-    'api.cronuts.com:8080',
-    'app.cronuts.com:8080',
-    'www.cronuts.com:8080',
-)
+CORS_ORIGIN_WHITELIST = ()
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 SESSION_COOKIE_NAME = 'myfatwallet'
 CSRF_COOKIE_NAME = 'csrftoken'
-CSRF_COOKIE_DOMAIN = SESSION_COOKIE_DOMAIN = '.cronuts.com'
+CSRF_COOKIE_DOMAIN = SESSION_COOKIE_DOMAIN = '.'
 CSRF_COOKIE_SECURE = SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = SESSION_COOKIE_HTTPONLY = False
